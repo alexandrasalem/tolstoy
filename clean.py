b5a5eef8a3d1b591d3857dae333aea6bafb95e98
Alex Salem <alexandrasalem8@gmail.com>
2018-06-12 14:29:37 -0700

    updating script, created new csvs

--- clean.py
+++ clean.py
@@ -1,18 +1,25 @@
 import csv
 from nltk import word_tokenize
 
+def clean(infilename, outfilename):
+	with open(infilename, "r") as f:
+		text = f.read()
 
-filename = "anna_karenina_clean.txt"
+	tokens = word_tokenize(text)
+	tokens = [w.lower() for w in tokens]
+	print(tokens[:100])
+	words = [word for word in tokens if word.isalpha()]
+	print(words[:100])
 
-with open(filename, "r") as f:
-	text = f.read()
+	with open(outfilename, "w") as csvfile:
+		writer = csv.writer(csvfile)
+		for word in words:
+			writer.writerow([word])
 
-tokens = word_tokenize(text)
-print(tokens[:100])
-words = [word for word in tokens if word.isalpha()]
-print(words[:100])
+names = [["anna_karenina_clean.txt", "anna_karenina.csv"], ["boyhood_clean.txt", "boyhood.csv"], 
+		["childhood_clean.txt", "childhood.csv"], ["cossacks_clean.txt", "cossacks.csv"],
+		["resurrection_clean.txt", "resurrection.csv"], ["war_and_peace_clean.txt", "war_and_peace.csv"],
+		["youth_clean.txt", "youth.csv"]]
 
-with open("new.csv", "w") as csvfile:
-	writer = csv.writer(csvfile)
-	writer.writerow(words)
-
+for name in names:
+	clean(name[0], name[1])
